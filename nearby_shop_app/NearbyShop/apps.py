11129fd48ab31744ecbc564345b45fef2dcba4f7
ALAHASSA Généreux <wilofice@gmail.com>
2018-11-21 22:37:59 +0100

    Create project base structure

---
+++ nearby_shop_app/NearbyShop/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NearbyshopConfig(AppConfig):
+    name = 'NearbyShop'
---
+++ nearby_shop_app/NearbyShop/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+
+
+class MyUser(models.Model):
+    pass
+# Create your models here.
